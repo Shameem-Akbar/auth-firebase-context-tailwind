2f70eeda21f71b76de75f8e64331423a07803632
Shameem-Akbar <Shameemakbar789@gmail.com>
2023-04-24 00:41:55 +0600

    implimented login and registration

--- src/__steps__.js
+++ src/__steps__.js
@@ -4,4 +4,16 @@
  * 2. create provider
  * 3. set context value
  * 4. to excess the value: useContext hook
- * **/ 
+ * **/
+
+/***
+ * For Sharing:
+ * 1. main.jsx set AuthProvider
+ * 2. inside AuthProvider access children props and then use
+ * */
+
+/***
+ * 1. breakdown the create user function
+ * 2. auth provider will have auth and pass email and password from register
+ * 
+ * */ 
--- src/providers/AuthProviders.jsx
+++ src/providers/AuthProviders.jsx
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { createContext } from 'react';
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import app from '../firebase/firebase.config';
 
-const AuthContext = createContext(null);
+export const AuthContext = createContext(null);
 
-const AuthProviders = () => {
+
+const auth = getAuth(app);
+
+const AuthProviders = ({ children }) => {
+
+    const [user, setUser] = useState(null);
+
+    const createUser = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    const signIn = (email, password) => {
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    const authInfo = {
+        user, createUser, signIn
+    }
     return (
-        <div>
-
-        </div>
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
     );
 };
 
